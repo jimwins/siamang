8ddf413d269806fab47e198e67a441bb166c859d
jim winstead <jimw@trainedmonkey.com>
2013-11-02 13:10:06 -0700

    Add setting of connection parameters, display of prompt with variables

--- siamang.js
+++ siamang.js
@@ -1,15 +1,60 @@
 var systemModel= function() {
   var self= this;
 
-  self.prompt= ko.observable('mysql>');
+  self.prompt= ko.observable('\\U:\\d mysql>');
   self.cmd= ko.observable('');
+
+  self.user= ko.observable('');
+  self.password= ko.observable('');
+  self.database= ko.observable('');
+  self.host= ko.observable('localhost');
 
   self.results= ko.observableArray();
 
+  self.displayprompt= ko.computed(function() {
+    var prompt= self.prompt().replace(/\\h/g, self.host())
+                             .replace(/\\u/g, self.user())
+                             .replace(/\\d/g, self.database())
+                             .replace(/\\U/g,
+                                      self.user() + '@' + self.host());
+    return prompt;
+  }, this);
+
   self.run= function(form) {
-    $.getJSON("db.php?callback=?",
-              { cmd: this.cmd() },
-              self.addResult);
+    var cmd= this.cmd();
+    var space= cmd.indexOf(' ');
+    if (space < 0)
+      space= cmd.length;
+
+    switch (cmd.substring(0, space)) {
+    case 'connect':
+      var space2= cmd.indexOf(' ', space + 1);
+      if (space2 < 0) space2= cmd.length;
+      this.user(cmd.substring(space + 1, space2));
+      if (space != space2)
+        this.password(cmd.substring(space2 + 1));
+      break;
+
+    case 'prompt':
+      var prompt= cmd.substring(space + 1);
+      if (!prompt) prompt= "mysql>";
+      this.prompt(prompt);
+      break;
+
+    case 'use':
+      var database= cmd.substring(space + 1);
+      if (!database) break;
+      this.database(database);
+      break;
+
+    default:
+      $.getJSON("db.php?callback=?",
+                { user: this.user(),
+                  password: this.password(),
+                  database: this.database(),
+                  cmd: this.cmd() },
+                self.addResult);
+    }
   };
 
   self.addResult= function(data) {
